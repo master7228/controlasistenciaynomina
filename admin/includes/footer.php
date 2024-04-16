3ee75286deede4a1b70939fbd2b3f625e7fd1364
master7228 <jbmboxer@gmail.com>
2024-04-16 10:08:46 -0500

    Initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>By <a href="">Mentes Brillantes</a></b>
+    </div>
+    <strong>Copyright &copy; 2024 Control de Asistencia y Sistema de Nómina </strong>
+</footer>
